66fea03734539a64ea4b9f69a7200fb761786482
devland <adevland@gmail.com>
2024-06-07 09:29:41 +0200

    JI-3998 update file structure for backwards compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -10,23 +10,19 @@ module.exports = {
       patterns: [
         {
           from: path.join(path.resolve(__dirname, 'node_modules'), 'tether', 'dist', 'js', 'tether.min.js'),
-          to: path.join(path.resolve(__dirname, 'dist'), 'tether.min.js')
+          to: path.join(path.resolve(__dirname, 'scripts'), 'tether.min.js')
         },
         {
           from: path.join(path.resolve(__dirname, 'node_modules'), 'tether', 'dist', 'css', 'tether.min.css'),
-          to: path.join(path.resolve(__dirname, 'dist'), 'tether.min.css')
+          to: path.join(path.resolve(__dirname, 'styles'), 'tether.min.css')
         },
         {
           from: path.join(path.resolve(__dirname, 'node_modules'), 'shepherd', 'dist', 'js', 'shepherd.min.js'),
-          to: path.join(path.resolve(__dirname, 'dist'), 'shepherd.min.js')
+          to: path.join(path.resolve(__dirname, 'scripts'), 'shepherd.min.js')
         },
         {
           from: path.join(path.resolve(__dirname, 'node_modules'), 'shepherd', 'dist', 'css', 'shepherd-theme-arrows.css'),
-          to: path.join(path.resolve(__dirname, 'dist'), 'shepherd-theme-arrows.css')
-        },
-        {
-          from: path.join(path.resolve(__dirname, './'), 'tether-shepherd.min.js'),
-          to: path.join(path.resolve(__dirname, 'dist'), 'tether-shepherd.min.js')
+          to: path.join(path.resolve(__dirname, 'styles'), 'shepherd-theme-arrows.css')
         }
       ]
     })
